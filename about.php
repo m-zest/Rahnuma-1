dc7cfb0159c5f25f0a841786620ebb3920bad1b1
Jiten Kumar Grover <39979352+JitenKumarGrover@users.noreply.github.com>
2020-08-03 15:57:47 +0530

    Add files via upload

--- about.php
+++ about.php
@@ -195,25 +195,5 @@
 		</div>
 	</div>
 
-	<div id="fh5co-gallery" class="fh5co-bg-section">
-		<div class="row text-center">
-			<h2><span>Instagram Gallery</span></h2>
-		</div>
-		<div class="row">
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-5.jpg);"></a>
-			</div>
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-2.jpg);"></a>
-			</div>
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-3.jpg);"></a>
-			</div>
-			<div class="col-md-3 col-padded">
-				<a href="#" class="gallery" style="background-image: url(images/project-4.jpg);"></a>
-			</div>
-		</div>
-	</div>
-
 	<?php include("front/footer.php") ?>
 
